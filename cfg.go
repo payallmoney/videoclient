18f77e714911aca52ade1460a9858ad8087f3cd9
oyx <44550777@qq.com>
2016-04-13 23:23:54 +0800

    1,改为正式配置

--- cfg.go
+++ cfg.go
@@ -6,6 +6,6 @@ func Cfg() map[string]interface{} {
 	//ret["server"] = "127.0.0.1:3000"
 	ret["server"] = "121.40.199.41:3000"
 	ret["kodi"] = "localhost:8080"
-	ret["checktime"] = time.Second*10
+	ret["checktime"] = time.Minute*10
 	return ret;
 }
