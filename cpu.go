95b863211f9677d80432e1f0b3324c49bdcb7ee1
oyx <44550777@qq.com>
2016-04-23 04:15:29 +0800

    1,修改cpuid使树莓派可用

--- cpu.go
+++ cpu.go
@@ -4,35 +4,38 @@ import (
 //"runtime"
 //"os/exec"
 //"strings"
-	"github.com/shirou/gopsutil/cpu"
+//	"github.com/shirou/gopsutil/cpu"
+"strings"
+	"os/exec"
+	"runtime"
 )
 
 /*
 	https://github.com/MetaScale/nt2/blob/a1ede0dc54a332dcc265e1cfed267d4b4a57e019/tools/is_multicore/is_multicore.cpp
 */
 //
-//func cpuid() string {
-//	cpuid := ""
-//	if runtime.GOOS == "windows" {
-//		// "wmic cpu get ProcessorId /format:csv"
-//		cmd :=exec.Command("cmd", "/c" ,"wmic cpu get ProcessorId /format:csv")
-//
-//		strs, err := cmd.Output()
-//		checkerr(err)
-//
-//		id := strings.Split(strings.Split(string(strs), "\r\n")[2], ",")[1];
-//		cpuid = strings.TrimSpace(id)
-//
-//		checkerr(err)
-//	}else {
-//		// "cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2"
-//		strs,_ :=exec.Command("bash","-c","cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2").Output()
-//		cpuid = strings.TrimSpace(string(strs))
-//	}
-//	return cpuid
-//}
+func cpuid() string {
+	cpuid := ""
+	if runtime.GOOS == "windows" {
+		// "wmic cpu get ProcessorId /format:csv"
+		cmd :=exec.Command("cmd", "/c" ,"wmic cpu get ProcessorId /format:csv")
 
-func cpuid()string{
-	ids,_ := cpu.Info()
-	return ids[0].PhysicalID
+		strs, err := cmd.Output()
+		checkerr(err)
+
+		id := strings.Split(strings.Split(string(strs), "\r\n")[2], ",")[1];
+		cpuid = strings.TrimSpace(id)
+
+		checkerr(err)
+	}else {
+		// "cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2"
+		strs,_ :=exec.Command("bash","-c","cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2").Output()
+		cpuid = strings.TrimSpace(string(strs))
+	}
+	return cpuid
 }
+
+//func cpuid()string{
+//	ids,_ := cpu.Info()
+//	return ids[0].PhysicalID
+//}
